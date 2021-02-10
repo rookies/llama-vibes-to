24224c5e4564aef82754f0905c94ea31bdd3cd40
Robert Knauer <robert@capsaicin-dev.de>
2021-02-10 22:18:19 +0100

    vibe.py: send commands via serial port

--- vibe.py
+++ vibe.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import threading
+import serial
 
 
 class Main:
@@ -9,6 +10,19 @@ class Main:
     _timer = None
     _abortVibingEvent = threading.Event()
     _debug = False
+    _angles = (10, 30)
+
+    def __init__(self, argv):
+        if len(argv) >= 2:
+            print('Opening serial port %s ...' % argv[1])
+            self._serial = serial.Serial(argv[1], 9600, timeout=0)
+        else:
+            self._serial = None
+
+    def __del__(self):
+        if self._serial is not None:
+            print('Closing serial port ...')
+            self._serial.close()
 
     def run(self):
         for line in sys.stdin:
@@ -43,9 +57,13 @@ class Main:
         self._abortVibingEvent.clear()
 
         # Vibe!:
+        self.serialSend("%d,%d\n" % (
+            self._angles[0], round(downDuration * 1000)))
         self.llamaNormal()
         if self._abortVibingEvent.wait(downDuration):
             return
+        self.serialSend("%d,%d\n" % (
+            self._angles[1], round(upDuration * 1000)))
         self.llamaVibe()
         if self._abortVibingEvent.wait(upDuration):
             return
@@ -71,6 +89,15 @@ class Main:
         print("  || ||")
         print("  '' ''")
 
+    def serialSend(self, msg):
+        if self._debug:
+            print(msg)
+        if self._serial is not None:
+            recvMsg = self._serial.readline()
+            if self._debug:
+                print(recvMsg.decode('ascii').strip())
+            self._serial.write(msg.encode('ascii'))
+
 
 if __name__ == '__main__':
-    Main().run()
+    Main(sys.argv).run()
